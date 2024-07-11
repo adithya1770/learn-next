c7b70a4f076e8c3d4f9a005b84f93ffe1c0df465
adithya1770 <adithyaps929@gmail.com>
2024-07-11 16:49:47 +0530

    added landing and home page

--- week-1/src/app/page.jsx
+++ week-1/src/app/page.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
+import './page.css'
 
 const page = () => {
   return (
-    <>
-    <h1 className='pl-96 text-9xl text-blue-500 hover:text-red-800'>Adithya P S</h1>
-    <div className='text-4xl bg-white text-black ml-82 mt-52 rounded-xl hover:text-white hover:bg-black'>
-      <p>confused on how to describe about myself</p>
-      <p>I've learnt how to create dynamic and static routes, creating a next app.</p>
+    <div>
+    <head>
+      <title>
+        Adithya P S
+      </title>
+    </head>
+    <h1 className='pl-96 text-9xl text-white font-bold font-mono'>Adithya P S</h1>
+    <div className='text-3xl text-white ml-82 mt-24 font-mono'>
+    <div className='tracking-wide absolute left-5'>
+      <p className='italic'>I am a proactive and inquisitive individual who is deeply involved in programming and technology. Adithya is currently a computer science student about to finish his freshman year and is keen on upskilling himself during his holidays. I've learnt how to create dynamic and static routes, creating a next app.I've learnt how to create dynamic and static routes, creating a next app.</p>
     </div>
-    </>
+    <br />
+    <div className='absolute right-44 bottom-5 h-64 w-64 moving'>
+      <img src="https://png.pngtree.com/png-vector/20240309/ourmid/pngtree-developers-are-coding-programs-on-computers-programmers-are-analyzing-data-png-image_11902650.png" alt="https://img.lovepik.com/element/45009/1966.png_860.png" />
+    </div>
+  </div>
+  </div>
+
   )
 }
 
