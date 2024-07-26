323c1f948253a78a6de413d942316d6c1447dcaa
adithya1770 <adithyaps929@gmail.com>
2024-07-26 12:43:12 +0530

    added error and loading pages

--- week-1/src/components/Navbar.jsx
+++ week-1/src/components/Navbar.jsx
@@ -4,8 +4,8 @@ const Navbar = () => {
   return (
     <div>
       <div className='text-3xl text-white h-10 w-72 bg-white ml-6 rounded-xl'>
-        <a href="/home" className='pl-6 hover:text-blue-500 hover:text-4xl text-black'>Home</a>
-        <a href="/products" className='pl-6 hover:text-blue-500 hover:text-4xl text-black'>Products</a>
+        <a href="/home" className='pl-6 hover:text-blue-500 hover:text-4xl text-black font-mono'>Home</a>
+        <a href="/products" className='pl-8 hover:text-blue-500 hover:text-4xl text-black font-mono'>Products</a>
       </div>
     </div>
   )
